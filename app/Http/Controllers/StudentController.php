4a6a8af3ea952ef46001dd1ad5bbfbe7ce3ed19e
FilipMilovanovic <coficofi4@gmail.com>
2022-09-19 20:09:39 +0200

    Pretraga studenata

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -95,4 +95,25 @@ class StudentController extends Controller
                 'rez' => 400
             ]);
     }
+
+
+
+
+    public function pretragaStudenata($pretragaPolje)
+    {
+        $studenti = DB::table('users')
+            ->where('name', 'like', "%$pretragaPolje%")
+            ->orWhere('broj_indeksa', 'like', "%$pretragaPolje%")
+            ->orWhere('email', 'like', "%$pretragaPolje%")
+            ->orWhere('status', 'like', "%$pretragaPolje%")
+            ->orWhere('smer', 'like', "%$pretragaPolje%")
+            ->get();
+
+
+        return response()->json([
+            'rez' => 200,
+            'studenti' => $studenti
+
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,7 @@ Route::get('sviStudenti', [StudentController::class, 'sviStudenti']);
 Route::delete('obrisiStudenta/{id}', [StudentController::class, 'obrisiStudenta']);
 Route::post('sacuvajStudenta', [StudentController::class, 'sacuvajStudenta']);
 Route::post('sacuvajIzmene/{brojIndeksa}', [StudentController::class, 'sacuvajIzmene']);
+Route::get('pretragaStudenata/{pretragaPolje}', [StudentController::class, 'pretragaStudenata']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
